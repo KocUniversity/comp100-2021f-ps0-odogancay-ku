10dd1114ce5708c1ed4a08adf9320308a7cb8977
odogancay-ku <91738862+odogancay-ku@users.noreply.github.com>
2021-10-01 13:30:13 +0000

    Changed the prompt text of input requests according to the isntructions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-x = int(input("Please enter the first number:\n"))
-y = int(input("Please enter the second number:\n"))
+x = int(input("Please enter number x:\n"))
+y = int(input("Please enter number y:\n"))
 print(x**y)
 print(math.log(x)/math.log(2))
 print("79734")
